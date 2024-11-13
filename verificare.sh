33424c153a5629ac48f5ac210d3a423f216ccad8
Tudor Beloiu <beloiutudorandrei@gmail.com>
2024-11-13 13:09:35 +0200

    modificare program

--- verificare.sh
+++ verificare.sh
@@ -2,7 +2,7 @@
 
 cat /etc/hosts | while read -r line
 do
-   ip=$(echo $liine | awk '{print $1}')
+   ip=$(echo $line | awk '{print $1}')
    domain=$(echo $line | awk '{print $2}')
    nslookup_ip=$(nslookup "$domain" 2>/dev/null | grep 'Address' | tail -n 1 | awk '{print $2}')
    if [[ "$ip" == *.*  && "$domain" != "localhost" ]]
@@ -11,5 +11,6 @@ do
            then
               echo "Bogus IP for $domain in /etc/hosts !"
        fi
+   fi
 done
 
