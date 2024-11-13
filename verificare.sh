1c514ceb702b6e6d63fa0057e6bee35a2b7ccd18
Tudor Beloiu <beloiutudorandrei@gmail.com>
2024-11-13 12:47:36 +0200

    Verificare ip gresit

---
+++ verificare.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+cat /etc/hosts | while read -r line
+do
+   ip=$(echo $liine | awk '{print $1}')
+   domain=$(echo $line | awk '{print $2}')
+   nslookup_ip=$(nslookup "$domain" 2>/dev/null | grep 'Address' | tail -n 1 | awk '{print $2}')
+   if [[ "$ip" == *.*  && "$domain" != "localhost" ]]
+     then
+       if [[ "$ip" != "$nslookup_ip" ]]
+           then
+              echo "Bogus IP for $domain in /etc/hosts !"
+       fi
+done
+
